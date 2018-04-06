a73b48884ed9f750b71d17cd22110563fb641f00
pieroplus <gussunoyoyo@hotmail.co.jp>
2018-04-06 14:11:40 +0900

    Create-prototype-edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show] do
-  	resources :comments, only: [:create]
+  resources :prototypes, only: [:index, :new, :create, :show, :update, :edit] do
+    resources :comments, only: [:create]
   end
   resources :users, only: [:show, :edit, :update]
   resources :tags, only: :index
